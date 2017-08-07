6c2c4743483faf840a617b36c6a876514e5e14e1
Timvde <tim_vdeynde@hotmail.com>
2017-08-07 22:28:34 +0200

    Do i18n in a cleaner way

--- popup/localize.js
+++ popup/localize.js
@@ -1,8 +1,7 @@
-function apply_translation(tag_id, message_id) {
-  document.getElementById(tag_id).innerText = browser.i18n.getMessage(message_id);
+// Source: https://bugs.chromium.org/p/chromium/issues/detail?id=115800#c17
+window.onload = function() {
+  [].forEach.call(document.querySelectorAll('[data-i18n]'), function(el) {
+    el.innerHTML = browser.i18n.getMessage(el.getAttribute('data-i18n'));
+  });
 }
 
-window.onload = function() {
-  apply_translation("current-window-label", "currentWindow")
-  apply_translation("all-windows-label", "allWindows")
-}
